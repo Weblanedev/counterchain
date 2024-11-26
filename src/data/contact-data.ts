1d07e07f4b8cbd46aa9e9abe00977c12f07e0c23
Bunmi Oye <bunmioye09@gmail.com>
2024-11-26 14:13:13 +0100

    initial commit

---
+++ src/data/contact-data.ts
@@ -0,0 +1,11 @@
+import { ContactType } from '@/interFace/interFace';
+
+const contact_data: ContactType = {
+  phone: ['123-456-7890'],
+  email: ['random@example.com'],
+  address: ['123 Main Street, Any-town, USA'],
+  googleMapUrl:
+    'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29551.13751693757!2d-74.115804243905!3d40.85532782107006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1649153266682!5m2!1sen!2sbd',
+};
+
+export default contact_data;
