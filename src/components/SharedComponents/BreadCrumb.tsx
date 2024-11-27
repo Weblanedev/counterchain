51c6f11db6966c65c92663f520893ea4f3849e6a
Bunmi Oye <bunmioye09@gmail.com>
2024-11-27 11:33:08 +0100

    edit info to match service

--- src/components/SharedComponents/BreadCrumb.tsx
+++ src/components/SharedComponents/BreadCrumb.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import React from "react";
-import bgImg from "../../../public/assets/img/bg/page-title-bg.jpg";
+import Link from 'next/link';
+import React from 'react';
+import bgImg from '../../../public/assets/img/bg/contact_banner.jpg';
 
 interface propsType {
   title: string;
@@ -10,13 +10,26 @@ const BreadCrumb = ({ title }: propsType) => {
   return (
     <>
       <section
-        className="page-title-area"
-        style={{ backgroundImage: `url(${bgImg.src})` }}
+        className="page-title-area position-relative"
+        style={{
+          backgroundImage: `url(${bgImg.src})`,
+        }}
       >
-        <div className="container">
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            zIndex: 1,
+          }}
+        ></div>
+        <div className="container position-relative" style={{ zIndex: 2 }}>
           <div className="row">
             <div className="col-lg-12">
-              <div className="page-title-wrapper text-center">
+              <div className="page-title-wrapper text-center text-white">
                 <h1 className="page-title mb-10">{title}</h1>
                 <div className="breadcrumb-menu">
                   <nav
